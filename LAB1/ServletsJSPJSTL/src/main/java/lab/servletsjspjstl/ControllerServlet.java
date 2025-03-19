728e033032046a627799da596250ffb2db1442df
deptage <zuzialawn@gmail.com>
2025-03-19 12:44:54 +0100

    added all tasks except for the last one

--- LAB1/ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/ControllerServlet.java
+++ LAB1/ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/ControllerServlet.java
@@ -1,5 +1,10 @@
 package lab.servletsjspjstl;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
@@ -21,6 +26,17 @@ public class ControllerServlet extends HttpServlet {
         myBean.setForegroundColor( request.getParameter("foreColor") );
         myBean.setBackgroundColor( request.getParameter("backColor") );
         request.setAttribute("bean", myBean);
+
+        ArrayList members = new ArrayList();
+        members.add("John Lennon");
+        members.add("Paul McCartney");
+        members.add("Ringo Starr");
+        members.add("George Harrison");
+        request.setAttribute("members", members);
+        Calendar calDate = new GregorianCalendar();
+        calDate.set(1965,Calendar.SEPTEMBER,13);
+        Date releaseDate = calDate.getTime();
+        request.setAttribute("releaseDate", releaseDate);
 
         ServletContext ctx = this.getServletContext();
         RequestDispatcher dispatcher =
--- LAB1/ServletsJSPJSTL/src/main/webapp/yesterday.jsp
+++ LAB1/ServletsJSPJSTL/src/main/webapp/yesterday.jsp
@@ -5,8 +5,11 @@
   Time: 12:30
   To change this template use File | Settings | File Templates.
 --%>
+<%@taglib uri="jakarta.tags.core" prefix="c"%>
+<%@taglib uri="jakarta.tags.fmt" prefix="fmt"%>
 <%@page contentType="text/html"%>
 <%@page pageEncoding="UTF-8"%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,6 +36,18 @@ Why she<br/>
 Had to go I don't know, she wouldn't say.<br/>
 I said,<br/>
 Something wrong, now I long for yesterday.<br/>
+
+<p>
+    Released: <fmt:formatDate value="${releaseDate}" type="date" dateStyle="full"/>
+</p>
+<c:if test="${!empty members}">
+    <table border>
+        <tr><th>The Beatles</th></tr>
+        <c:forEach var="member" items="${members}">
+            <tr><td>${member}</td></tr>
+        </c:forEach>
+    </table>
+</c:if>
 
 <a href="yesterday.html">Back</a>
 </body>
