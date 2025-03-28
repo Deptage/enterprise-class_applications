07bcf723a36494a72c81e414e55feea019d79740
deptage <zuzialawn@gmail.com>
2025-03-28 11:17:27 +0100

    po zadaniu 29

--- LAB2/JSFLogin/src/main/java/lab/backing/LoginBean.java
+++ LAB2/JSFLogin/src/main/java/lab/backing/LoginBean.java
@@ -1,12 +1,18 @@
 package lab.backing;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.component.html.HtmlCommandButton;
+import jakarta.faces.component.html.HtmlSelectBooleanCheckbox;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ValueChangeEvent;
 import jakarta.inject.Named;
 
 @RequestScoped @Named
 public class LoginBean {
     private String username;
     private String password;
+    private HtmlSelectBooleanCheckbox acceptCheckbox;
+    private HtmlCommandButton loginButton;
 
     public String getUsername() {
         return username;
@@ -30,5 +36,34 @@ public class LoginBean {
         } else {
             return "failure";
         }
+    }
+
+    public HtmlSelectBooleanCheckbox getAcceptCheckbox() {
+        return acceptCheckbox;
+    }
+
+    public void setAcceptCheckbox(HtmlSelectBooleanCheckbox acceptCheckbox) {
+        this.acceptCheckbox = acceptCheckbox;
+    }
+
+    public HtmlCommandButton getLoginButton() {
+        return loginButton;
+    }
+
+    public void setLoginButton(HtmlCommandButton loginButton) {
+        this.loginButton = loginButton;
+    }
+
+
+    public void activateButton(ValueChangeEvent e)
+    {
+        if (acceptCheckbox.isSelected())
+            loginButton.setDisabled(false);
+        else
+            loginButton.setDisabled(true);
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.renderResponse();
+
     }
 }
