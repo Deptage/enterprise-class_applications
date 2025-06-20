ab8398a4ad3c068ad8317abe4ec9e3ce878c54cf
deptage <zuzialawn@gmail.com>
2025-06-20 13:36:45 +0200

    po zad 9

--- LAB10/react_proj/src/ProductList.js
+++ LAB10/react_proj/src/ProductList.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProductList = ({}) =>{
+    const [products, setProducts] =useState([]);
+        useEffect(() => {
+        setProducts([
+            {id: 1, title: "iPhone 14", brand: "Apple"},
+            {id: 2, title: "iPad Air", brand: "Apple"},
+            {id: 3, title: "Galaxy A51", brand: "Samsung"}
+        ]);
+    }, []);
+
     return (
-    <h1>List of Products</h1>
+        <div>
+            <h1>List of Products</h1>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        {product.title} ({product.brand})
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
