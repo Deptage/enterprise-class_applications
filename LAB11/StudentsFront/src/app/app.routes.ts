d1e51c25f23263854a93dd7bf224ba3bdd7578eb
deptage <zuzialawn@gmail.com>
2025-06-21 13:05:44 +0200

    skonczony tutorial nr 11 yay

--- LAB11/StudentsFront/src/app/app.routes.ts
+++ LAB11/StudentsFront/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { StudentsComponent } from './students/students.component';
 import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/students', pathMatch: 'full' },
     { path: 'students', component: StudentsComponent },
-    { path: 'student-detail/:id', component: StudentDetailComponent }
+    { path: 'student-detail/:id', component: StudentDetailComponent },
+    {path: 'about', component: AboutComponent}
      
 ];
